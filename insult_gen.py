dfc93789fed421708f3b7173797cdbeb888fa418
Sebastian Reel <64790218+sebastianreel@users.noreply.github.com>
2021-05-06 23:06:40 +0000

    file

---
+++ insult_gen.py
@@ -0,0 +1,27 @@
+# Sebastian Reel
+# Python Practice
+# Randomized Insult Generator
+#-----------------------------#
+
+# first get the user's name and age for insult parameteres
+print("What is your name? ")
+name = input(name)
+print("Hello " + name)
+print()
+
+print("What is your age, " + name + "?")
+age = int(input("Age: "))
+print()
+
+# now define a function with insults and parameters
+from random import choice
+def printInsult (name, age):
+  adjectives = ["big", "poopy", "dumb", "silly", "stinky", "cranky", "weird", "trashy"]
+  nouns = ["turnip", "dog", "fart", "hairball", "chicken", "wet-wipe", "hag", "whale"]
+  if(age < 16):
+    ageA = "young "
+    print (name + ", you are a " + ageA + choice(adejctives) + " " + chioce(nouns))
+  else:
+    ageA = "old "
+    print (name + ", you are an " + ageA + choice(adjectives) + " " + choice(nouns))
+printInsult(name, age)
